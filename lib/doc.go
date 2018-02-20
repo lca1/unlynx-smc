f59c2d19c41aa3dd7c1d5afd67776e4e6e76c0fb
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-02-20 11:21:52 +0100

    Copied prioUtils

---
+++ lib/doc.go
@@ -0,0 +1,6 @@
+package libUnLynxSMC
+
+/**
+Most of the function here literally copy/cut from Prio. Others are also copy cut with modifications
+due to the usage of some structs not needed in UnLynx.
+*/
